dd02cfc921f2efcf7b8100abd255e48d4dc7dd85
HassanELsherbini <h.elsherbini7@gmail.com>
2018-01-12 11:59:40 -0500

    add to cart functionality

---
+++ server/utility/cart.js
@@ -0,0 +1,8 @@
+export function addToCart(product, cart) {
+  if (!cart[product.id]){
+    product.quantity = 1;
+    cart[product.id] = product;
+  } else {
+    cart[product.id].quantity++;
+  }
+}
